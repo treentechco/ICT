bafd7df0f56afe7f831e05744b9100c469597f72
treentechco <treentechco@gmail.com>
2026-01-03 14:17:43 +0000

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -355,8 +355,8 @@ export default function ICTForexLandingPage() {
               </p>
               <a
              href="https://www.treentech.co/"
-             className="mt-4 rounded-full border border-slate-700 px-4 py-1.5 text-xs font-semibold hover:border-emerald-400/70 hover:text-emerald-200"
-             target=" mt-4 rounded-full border border-slate-700 px-4 py-1.5 text-xs font-semibold hover:border-emerald-400/70 hover:text-emerald-200"
+             className="mt-4 rounded-full bg-emerald-500 border-slate-700 px-4 py-1.5 text-xs font-semibold hover:border-emerald-400/70 hover:text-emerald-200"
+             target=" mt-4 rounded-full border border-slate-700 px-4 py-1.5 text-xs font-bold hover:border-emerald-400/70 hover:text-emerald-200"
              rel="noopener noreferrer"
              >
                 Explore Core ICT Modules  
@@ -685,6 +685,7 @@ export default function ICTForexLandingPage() {
     </div>
   );
 }
+
 
 
 
